813ef11dd62b56687f38020f727aea97df2d6d26
Fred <contact.hammami.fredj@gmail.com>
2020-06-06 20:27:18 +0200

    Add comments

--- DiagonalDifference.js
+++ DiagonalDifference.js
@@ -1,3 +1,7 @@
+/** Given a square matrix (arr), calculate the absolute difference
+* between the sums of its diagonals.
+* Time complexity here is O(n).
+*/
 function solve(arr, sum = 0, l = arr.length) {
     for (let i = 0; i < l; i++) {
         sum += arr[i][i] - arr[i][l - i - 1];
